11f486d9dbc2e6ca2f821e947fbc3c27bde937a7
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2014-03-09 11:38:18 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysulit',
+      version='1.0',
+      description='Unofficial Sulit.com.ph SDK for Python',
+      author='Ben Sarmiento',
+      author_email='me@bensarmiento.com',
+      url='https://github.com/yowmamasita/pysulit',
+      py_modules=['pysulit.api'],
+      requires=['requests', 'beautifulsoup4']
+      )
